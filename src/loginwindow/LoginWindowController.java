00b5df78099db0679062345c4c584f5ddf4a5abb
AbhilashG97 <abhilash.ag1997@gmail.com>
2019-05-01 20:54:53 +0530

    Student CourseView complete :sparkles:

--- src/loginwindow/LoginWindowController.java
+++ src/loginwindow/LoginWindowController.java
@@ -5,8 +5,10 @@ package loginwindow;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Paths;
@@ -34,10 +36,11 @@ import utilities.MockData;
  * @author Abhilash G <abhilashg@am.students.amrita.edu>
  */
 public class LoginWindowController implements Initializable {
-    
+
     @FXML private TextField usernameTextField;
     @FXML private PasswordField passwordField;
     @FXML private Button loginButton;
+    @FXML private Student selectedStudent;
     
     /**
      * Initializes the controller class
@@ -45,37 +48,52 @@ public class LoginWindowController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         onLoginButtonClicked();
-    }    
-    
+    }
+
     public void onLoginButtonClicked() {
         loginButton.setOnAction((event) -> {
-            if(isUserAuthenticated()) {
-                openStudentDashBoard(event); 
+            if (isUserAuthenticated()) {
+                try {
+                    writeToFile();
+                } catch (IOException ex) {
+                    Logger.getLogger(LoginWindowController.class.getName())
+                            .log(Level.SEVERE, null, ex);
+                }
+                openStudentDashBoard(event);
             } else {
                 System.err.println("Ah oh, did you enter everything correctly?");
-            }      
-        });            
+            }
+        });
     }
-    
+
     /**
      * A method that authenticated the user
+     *
      * @return true if the user is authenticated else return false
      */
     private boolean isUserAuthenticated() {
+        
         ArrayList<Student> data = new MockData().readMockData();
         
-        return data.stream().anyMatch((student) -> (student.getPassword()
-                .equals(passwordField.getText().trim()) 
+        for(Student student : data) {
+            if(student.getPassword()
+                .equals(passwordField.getText().trim())
                 && student.getUsername()
-                        .equals(usernameTextField.getText().trim())));
+                        .equals(usernameTextField.getText().trim())) {
+                setSelectedStudent(student);
+                return true;
+            }
+        }
+        return false;
     }
-    
+
     /**
      * This is a helper method which opens the StudentDashboard Scene
+     *
      * @param event is needed to access the stage
      */
     private void openStudentDashBoard(ActionEvent event) {
-        
+
         URL url = null;
         try {
             url = Paths.get("src/studentdashboard/StudentDashboard.fxml")
@@ -84,20 +102,50 @@ public class LoginWindowController implements Initializable {
             Logger.getLogger(LoginWindowController.class.getName())
                     .log(Level.SEVERE, null, ex);
         }
+        FXMLLoader loader = new FXMLLoader();
+        
+        loader.setLocation(url);
         
         Parent parent = null;
         try {
-            parent = FXMLLoader.load(url);
+            parent = loader.load(url);
         } catch (IOException ex) {
             Logger.getLogger(LoginWindowController.class.getName())
                     .log(Level.SEVERE, null, ex);
         }
-        
+
         Scene studentDashboardScene = new Scene(parent);
-        
-        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(studentDashboardScene);
         stage.show();
-    }   
+    }
+    
+    private void setSelectedStudent(Student student) {
+        selectedStudent = student;
+    }
+    
+    private Student getSelectedStudent() {
+        return selectedStudent;
+    }
+    
+    private void writeToFile() throws IOException {
+        
+        String fileSeparator = System.getProperty("file.separator");
+    
+        String fileDataPath = "src" + fileSeparator + "data" 
+                + fileSeparator + "dashboard_data.ser";        
+        
+        File file = new File(fileDataPath);
+        
+        System.out.println("Data Written to : " + fileDataPath);
+        
+        file.createNewFile();
+        
+        ObjectOutputStream objectWriter = 
+                new ObjectOutputStream(new FileOutputStream(file));
+        
+        objectWriter.writeObject(getSelectedStudent());
+    }
+    
 }
-
--- src/studentdashboard/StudentDashboardController.java
+++ src/studentdashboard/StudentDashboardController.java
@@ -1,17 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package studentdashboard;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import pojo.Course;
+import pojo.Student;
+import utilities.MockData;
 
 /**
  * FXML Controller class
@@ -19,17 +23,76 @@ import javafx.scene.control.TextField;
  * @author Abhilash G <abhilashg@am.students.amrita.edu>
  */
 public class StudentDashboardController implements Initializable {
-    
-    @FXML private TextField semesterTextField;
-    @FXML private Button getSubjectsButton;
-    @FXML private ListView subjectListView;
-    
+
+    @FXML
+    private TextField semesterTextField;
+    @FXML
+    private Button getSubjectsButton;
+    @FXML
+    private ListView subjectListView;
+    private Student student;
+    private int selectedSemester;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
-    
+        student = readFromDashboardData();
+        onGetSubjectsButtonPressed();
+    }
+
+    public void onGetSubjectsButtonPressed() {
+        getSubjectsButton.setOnAction((event) -> {
+            selectedSemester = Integer.parseInt(semesterTextField.getText());
+            displayData(selectedSemester);
+        });
+    }
+
+    public void displayData(int semester) {
+
+        if (student.getCourseMap().containsKey(selectedSemester)) {
+            student.getCourseMap().forEach((key, value) -> {
+                if (Integer.parseInt(key.toString()) == selectedSemester) {
+                    for (Course course : value) {
+                        subjectListView.getItems().add(course.getCourseName());
+                    }
+                }
+            });
+
+        } else {
+            System.err.println("Ah oh, something is wrong!");
+            subjectListView.getItems().clear();
+        }
+    }
+
+    private Student readFromDashboardData() {
+
+        String fileSeparator = System.getProperty("file.separator");
+
+        String fileDataPath = "src" + fileSeparator + "data"
+                + fileSeparator + "dashboard_data.ser";
+
+        ObjectInputStream objectInputStream = null;
+        try {
+            objectInputStream
+                    = new ObjectInputStream(new FileInputStream(fileDataPath));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MockData.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(MockData.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+
+        Student selectedStudent = null;
+        try {
+            selectedStudent = (Student) objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(MockData.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+
+        return selectedStudent;
+    }
 }
