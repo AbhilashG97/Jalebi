b198e0d79a1d2f1f35dcd14d182d51046876b737
AbhilashG97 <abhilash.ag1997@gmail.com>
2019-05-01 23:57:22 +0530

    StudentCourseDetailWindow complete :sparkles:

---
+++ src/coursedetails/CourseDetailWindowController.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2019 Abhilash G <abhilashg@am.students.amrita.edu>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package coursedetails;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import loginwindow.LoginWindowController;
+import pojo.Course;
+
+/**
+ * FXML Controller class
+ *
+ * @author Abhilash G <abhilashg@am.students.amrita.edu>
+ */
+public class CourseDetailWindowController implements Initializable {
+    
+    @FXML private Label firstPeriodicalLabel;
+    @FXML private Label secondPeriodicalLabel;
+    @FXML private Label continousEvaluationLabel;
+    @FXML private Label endSemesterLabel;
+    @FXML private Label gradeLabel;
+    @FXML private Button backButton;
+    @FXML private GridPane gridPane;
+    
+    private Course course;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        onBackButtonPressed();
+    }    
+    
+    public void initializeData(Course course) {
+        this.course = course;
+        displayData();
+    }
+    
+    public void onBackButtonPressed() {
+        backButton.setOnAction((event) -> {
+            URL url = null;
+            try {
+                url = Paths.get("src/studentdashboard/StudentDashboard.fxml")
+                        .toUri().toURL();
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(LoginWindowController.class.getName())
+                        .log(Level.SEVERE, null, ex);
+            }
+            FXMLLoader loader = new FXMLLoader();
+
+            loader.setLocation(url);
+
+            Parent parent = null;
+            try {
+                parent = loader.load(url);
+            } catch (IOException ex) {
+                Logger.getLogger(LoginWindowController.class.getName())
+                        .log(Level.SEVERE, null, ex);
+            }
+
+            Scene studentDashboardScene = new Scene(parent);
+
+            Stage stage = (Stage) ((Node) event.getSource())
+                    .getScene().getWindow();
+            stage.setScene(studentDashboardScene);
+            stage.show();            
+        });
+    }
+    
+    private void displayData() {
+        
+        System.out.println(String.valueOf(course.getMarks()
+                .getFirstInternal()));
+        
+        firstPeriodicalLabel.setText(String.valueOf(course.getMarks()
+                .getFirstInternal()));
+        secondPeriodicalLabel.setText(String.valueOf(course.getMarks()
+                .getSecondInternal()));
+        continousEvaluationLabel.setText(String.valueOf(course.getMarks()
+                .getContinousEvaluationMarks()));
+        endSemesterLabel.setText(String.valueOf(course.getMarks()
+                .getEndSemester()));
+        gradeLabel.setText(String.valueOf(course.getMarks()
+                .getGrade()));
+        
+    }
+}
--- src/studentdashboard/StudentDashboardController.java
+++ src/studentdashboard/StudentDashboardController.java
@@ -1,18 +1,29 @@
 package studentdashboard;
 
+import coursedetails.CourseDetailWindowController;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import pojo.Course;
 import pojo.Student;
 import utilities.MockData;
@@ -40,6 +51,7 @@ public class StudentDashboardController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         student = readFromDashboardData();
         onGetSubjectsButtonPressed();
+        onListViewClickedTwice();
     }
 
     public void onGetSubjectsButtonPressed() {
@@ -66,6 +78,66 @@ public class StudentDashboardController implements Initializable {
         }
     }
 
+    public void onListViewClickedTwice() {
+        subjectListView.setOnMouseClicked((event) -> {
+        if(event.getButton().equals(MouseButton.PRIMARY)){
+            if(event.getClickCount() == 2){
+                System.out.println("Double clicked");
+                openCourseDetailWindow(event);                
+            }
+        }
+        });
+    }
+    
+    private void openCourseDetailWindow(MouseEvent event) {
+        
+        Course selectedCourse = null;
+        
+        String selectedCourseName = subjectListView.getSelectionModel()
+                .getSelectedItem().toString();
+        
+        ArrayList<Course> courseList = student.getCourseMap()
+                .get(selectedSemester);
+        
+        for(Course course : courseList) {
+            if(course.getCourseName().equals(selectedCourseName)) {
+                selectedCourse = course;
+            }
+        }
+        
+        System.out.println(selectedCourse);
+        
+        FXMLLoader loader = new FXMLLoader();
+        
+        URL url = null;
+        try {
+            url = Paths.get("src/coursedetails/CourseDetailWindow.fxml")
+                    .toUri().toURL();
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(StudentDashboardController.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        
+        loader.setLocation(url); 
+        Parent parent = null;
+        
+        try {
+            parent = loader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(StudentDashboardController.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        
+        Scene scene = new Scene(parent);
+        
+        CourseDetailWindowController controller = loader.getController();      
+        controller.initializeData(selectedCourse);
+        
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    
     private Student readFromDashboardData() {
 
         String fileSeparator = System.getProperty("file.separator");
@@ -95,4 +167,9 @@ public class StudentDashboardController implements Initializable {
 
         return selectedStudent;
     }
+    
+    public void restoreState() {
+        
+    }
+    
 }
